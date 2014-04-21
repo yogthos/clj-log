1667ebbd1c7086559bbd3e257aabc6f3f5dad4f3
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-04-21 14:34:15 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/yogthos/clj-log"  
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [log4j "1.2.17"]])
