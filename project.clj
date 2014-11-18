44912eb250a473fea7b0959341030fcb94ddef8e
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-11-18 07:51:33 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [log4j "1.2.17"]])
